35056fdf1975b6c82c2f448639c7ad3b3f5a7ffa
VicRVega <vicramirez@csumb.edu>
2024-04-04 23:19:59 -0700

    fixed issues in LoginActivity.java

--- app/src/main/java/com/example/project2_wellness_wizard_app/LoginActivity.java
+++ app/src/main/java/com/example/project2_wellness_wizard_app/LoginActivity.java
@@ -4,11 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 import com.example.project2_wellness_wizard_app.databinding.ActivityLoginBinding;
 
@@ -19,6 +16,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
     }
 
