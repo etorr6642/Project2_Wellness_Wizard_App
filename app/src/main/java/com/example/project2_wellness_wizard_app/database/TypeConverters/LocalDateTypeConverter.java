f51e893ead87199cfb605b6bbc7768b3677d8403
VicRVega <vicramirez@csumb.edu>
2024-04-04 22:23:20 -0700

    Added TypeConverter from Edwards branch

---
+++ app/src/main/java/com/example/project2_wellness_wizard_app/database/TypeConverters/LocalDateTypeConverter.java
@@ -0,0 +1,24 @@
+package com.example.project2_wellness_wizard_app.database.TypeConverters;
+
+import androidx.room.TypeConverter;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class LocalDateTypeConverter {
+    @TypeConverter
+    public long convertDateToLong(LocalDateTime date){
+        //uses whatever time is on the device
+        ZonedDateTime zdt = ZonedDateTime.of(date, ZoneId.systemDefault());
+        return zdt.toInstant().toEpochMilli();
+    }
+
+    @TypeConverter
+    public LocalDateTime convertLongToDate(Long epochMilli){
+        Instant instant = Instant.ofEpochMilli(epochMilli);
+        return LocalDateTime.ofInstant(instant,ZoneId.systemDefault());
+    }
+
+}
