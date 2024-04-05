7a5f39f3470deccc08be243bf9934b2d91737873
VicRVega <vicramirez@csumb.edu>
2024-04-05 00:14:19 -0700

    added setOnClickListener to logout button, it takes user bac to LoginActivity

--- app/src/main/java/com/example/project2_wellness_wizard_app/MainActivity.java
+++ app/src/main/java/com/example/project2_wellness_wizard_app/MainActivity.java
@@ -40,6 +40,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        binding.logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = LoginActivity.loginIntentFactory(getApplicationContext());
+                startActivity(intent);
+            }
+        });
+
 
     }
 
