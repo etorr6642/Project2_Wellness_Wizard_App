7c3ee43fae021fbe9297bd802707f7d12ce3adbc
Edward Torres <etorres5124@gmail.com>
2024-04-04 20:15:14 -0700

    Created layout for activity_login.xml updated graded for binding created Intent for LoginActivity called Intent in MainActivity for LoginActivity

--- app/src/main/java/com/example/project2_wellness_wizard_app/LoginActivity.java
+++ app/src/main/java/com/example/project2_wellness_wizard_app/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.project2_wellness_wizard_app;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.content.Intent;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +10,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.project2_wellness_wizard_app.databinding.ActivityLoginBinding;
+
 public class LoginActivity extends AppCompatActivity {
 
+    private ActivityLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+    }
+
+    static Intent loginIntentFactory(Context context){
+        return new Intent(context, LoginActivity.class);
     }
 }
