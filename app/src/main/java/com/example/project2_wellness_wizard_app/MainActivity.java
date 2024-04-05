c7fecd6b53c54299c9b11404dd9982d738408515
VicRVega <vicramirez@csumb.edu>
2024-04-04 18:12:44 -0700

    Added dependencies, worked on UserInfo, User, and started MainActivity.

--- app/src/main/java/com/example/project2_wellness_wizard_app/MainActivity.java
+++ app/src/main/java/com/example/project2_wellness_wizard_app/MainActivity.java
@@ -3,11 +3,25 @@ package com.example.project2_wellness_wizard_app;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.project2_wellness_wizard_app.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    public static final String TAG = "WELLNESS_WIZARD";
+
+    String mFood = "";
+    int mCalories = 0;
+    String vitMeds = "";
+    String mTimeOfDay = "";
+    double mWater = 0.0;
+    double mWeight = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
