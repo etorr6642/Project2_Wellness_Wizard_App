326d87db81f3bf1f9c1a3f902098a11d7ebe423e
VicRVega <vicramirez@csumb.edu>
2024-04-12 21:43:49 -0700

    Testing AddWorkoutActivity

--- app/src/main/java/com/example/project2_wellness_wizard_app/AddWorkoutActivity.java
+++ app/src/main/java/com/example/project2_wellness_wizard_app/AddWorkoutActivity.java
@@ -4,15 +4,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.project2_wellness_wizard_app.database.UserInfoRepository;
+import com.example.project2_wellness_wizard_app.database.entities.Workout;
 import com.example.project2_wellness_wizard_app.databinding.ActivityAddWorkoutBinding;
 
 public class AddWorkoutActivity extends AppCompatActivity {
     private ActivityAddWorkoutBinding binding;
     private UserInfoRepository repository;
+    private String mWorkout;
     public static final String TAG = "WELLNESS_WIZARD";
     private int loggedInUserId =-1 ;
     @Override
@@ -26,8 +30,35 @@ public class AddWorkoutActivity extends AppCompatActivity {
                 Context.MODE_PRIVATE);
         loggedInUserId = sharedPreferences.getInt(getString(R.string.preference_userId_key),-1);
 
+        binding.addWorkoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getWorkoutFromDisplay();
+                addWorkout();
+            }
+        });
 
+        binding.addWorkoutBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = AdminActivity.AdminActivityIntentFactory(getApplicationContext());
+                startActivity(intent);
+            }
+        });
     }
+
+    private void addWorkout(){
+        Workout workout = new Workout(mWorkout);
+        repository.addWorkout(workout);
+        toastMaker("Workout was successfully Added!");
+    }
+    private void getWorkoutFromDisplay(){
+        mWorkout = binding.addWorkoutInputEditText.getText().toString();
+    }
+    private void toastMaker(String message) {
+        Toast.makeText(this, message,Toast.LENGTH_SHORT).show();
+    }
+
     public static Intent AddWorkoutActivityIntentFactory(Context context){
         return new Intent(context, AddWorkoutActivity.class);
     }
